4f1ad9437e01d6b48f5308f2f27041fb2b0e3e35
xmariafdz <115629455+xmariafdz@users.noreply.github.com>
2023-03-07 11:27:42 +0100

    esqueleto

---
+++ src/main/java/org/mps/deque/DoublyLinkedListDeque.java
@@ -0,0 +1,50 @@
+package org.mps.deque;
+
+public class DoublyLinkedListDeque<T> implements DoubleEndedQueue<T> {
+
+    private DequeNode<T> first;
+    private DequeNode<T> last;
+    private int size;
+
+    public DoublyLinkedListDeque() {
+        // TODO
+    }
+
+    @Override
+    public void prepend(T value) {
+        // TODO
+    }
+
+    @Override
+    public void append(T value) {
+        // TODO
+    }
+
+    @Override
+    public void deleteFirst() {
+        // TODO
+    }
+
+    @Override
+    public void deleteLast() {
+        // TODO
+    }
+
+    @Override
+    public T first() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public T last() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO
+        return 0;
+    }
+}
