b4e805fc5ae7ca4cd77293a51e1e7751d192d916
xmariafdz <115629455+xmariafdz@users.noreply.github.com>
2023-03-08 16:44:58 +0100

    Hola

--- src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
+++ src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
@@ -1,10 +1,18 @@
 package org.mps.deque;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
 /**
  * @author Maria Fernández Moreno
  * @author Carlos Castaño Moreno
  */
 
-public class DoublyLinkedListDeque {
-    
+public class DoublyLinkedListDequeTest {
+
+    @Nested
+    @DisplayName("Tests for size method")
+    class QueuesSizeTests{
+
+    }
 }
