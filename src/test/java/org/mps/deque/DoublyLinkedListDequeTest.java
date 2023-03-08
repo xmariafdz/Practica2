2b97ac97d334e4e2683a75707705343406a7356c
xmariafdz <115629455+xmariafdz@users.noreply.github.com>
2023-03-08 17:11:32 +0100

    Update pom

--- src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
+++ src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
@@ -1,7 +1,9 @@
 package org.mps.deque;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 /**
  * @author Maria Fernández Moreno
@@ -9,10 +11,36 @@ import org.junit.jupiter.api.Nested;
  */
 
 public class DoublyLinkedListDequeTest {
-
+    DoublyLinkedListDeque listDeque;
+    @BeforeEach
+    public void beforeEach(){
+        listDeque = new DoublyLinkedListDeque();
+    }
+    @AfterEach
+    void shutdown() {
+        listDeque = new DoublyLinkedListDeque<>();
+    }
     @Nested
     @DisplayName("Tests for size method")
     class QueuesSizeTests{
 
+        @Test
+        public void sizeOfAnEmptyListIsZero(){
+            assertEquals(listDeque.size(),0);
+        }
+
+        @Test
+        public void sizeOfAnOneItemListIsOne(){
+            listDeque.append(1);
+            assertEquals(listDeque.size(),1);
+        }
+
+        @Test
+        public void sizeOfATwoItemsListIsThree(){
+            listDeque.append(1);
+            listDeque.append(2);
+            listDeque.append(3);
+            assertEquals(listDeque.size(),3);
+        }
     }
 }
